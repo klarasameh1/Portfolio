ead6660992e9b5a691988de2eb74bf7a596b7e5f
klara sameh <klarasameh39@gmail.com>
2025-09-10 20:17:53 +0300

    main structure for projects

---
+++ lib/pages/Projects/projectsPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:my_portfolio/widgets/rectangleBox.dart';
+
+class ProjectsPage extends StatelessWidget {
+  const ProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Section header
+            Center(child: RectangleBox(content: "About Me")),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
