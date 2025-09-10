a3affe8a9d9f7011db535e522090c2c3e4bc06d4
klara sameh <klarasameh39@gmail.com>
2025-09-10 18:28:27 +0300

    edit in about text

--- lib/pages/About/aboutPage.dart
+++ lib/pages/About/aboutPage.dart
@@ -42,26 +42,15 @@ class AboutPage extends StatelessWidget {
             const Text(
               textAlign: TextAlign.center,
               "I’ve worked on projects ranging from personal portfolio to task management app and mini shopping app, "
-                  "\npracticing state management with Provider, I also pay close attention to UI/UX principles to make applications both functional and beautiful"
-                  'My goal is to grow as a developer by contributing to impactful projects while continuously learning and improving my craft.',
+                  "\npracticing state management with Provider\n I also pay close attention to UI/UX principles to make applications both functional and beautiful"
+                  '\nMy goal is to grow as a Flutter developer by contributing to impactful projects while continuously learning and improving my craft.',
               style: TextStyle(
                 fontSize: 22,
                 color: Colors.white,
                 height: 1.5,
               ),
             ),
-            const SizedBox(height: 20),
 
-            // Goal
-            const Text(
-              "My goal is to contribute to projects that make a real difference "
-                  "while continuously improving as a Flutter developer.",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                height: 1.5,
-              ),
-            ),
           ],
       ),
     );
