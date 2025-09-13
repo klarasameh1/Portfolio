4604d2f193301065df30a37aaec27e3d85e9b8f9
klara sameh <klarasameh39@gmail.com>
2025-09-13 18:50:03 +0300

    edit box

--- lib/widgets/rectangleBox.dart
+++ lib/widgets/rectangleBox.dart
@@ -14,31 +14,22 @@ class RectangleBox extends StatelessWidget {
       width: 250,
       height: 80,
       decoration: BoxDecoration(
-        color: Colors.transparent,
-        border: Border(
-          right: BorderSide(
-              color: Colors.white70, // softer border
-              width: 3,
-          ),
-          left: BorderSide(
-            color: Colors.white70, // softer border
-            width: 3,
-          ),
+        borderRadius: BorderRadius.circular(25), // slight rounding
+        border: Border.all(
+          color: Colors.white70,
+          width: 2,
         ),
       ),
-      child:  Center(
+      child: Center(
         child: Text(
           content,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
-            fontSize: 28,
+            fontSize: 24,
           ),
         ),
       ),
     );
   }
 }
-
-
-
